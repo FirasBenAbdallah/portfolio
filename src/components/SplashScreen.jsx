e08f225f01dcc205d0470497010aac414b57ce98
FirasBenAbdallah <firas.benabdallah@esprit.tn>
2023-11-03 14:12:14 +0100

    Splash

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -1,15 +1,15 @@
+import React from "react";
+import palestine from "../assets/images/palestine.png";
+import fba from "../assets/images/signature_FBA_Black.png";
+
 const SplashScreen = () => {
   return (
     <>
       <div className="welcome-container">
-        <img
-          src="src/assets/images/palestine.png"
-          alt="Palestine"
-          className="top-right-image"
-        />
+        <img src={palestine} alt="Palestine" className="top-right-image" />
         <h1 className="welcome-title">Welcome/Bienvenue</h1>
         <p className="welcome-subtitle">To</p>
-        <img src="src/assets/images/signature_FBA_Black.png" alt="FBA" className="fba-img"/>
+        <img src={fba} alt="FBA" className="fba-img" />
         <p className="welcome-subtitle">Portfolio</p>
       </div>
     </>
